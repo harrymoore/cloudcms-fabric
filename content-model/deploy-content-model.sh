11ca4c324b09df186e9d93f75b62678b05232783
Harry Moore <harrymoore@yahoo.com>
2020-05-21 17:41:23 -0400

    content model updates

--- content-model/deploy-content-model.sh
+++ content-model/deploy-content-model.sh
@@ -5,4 +5,5 @@
 
 # use cloudcms-util import feature to upload a local copy of the content model to a cloud cms branch
 # npm install cloudcms-util
-npx cloudcms-util import -g ../gitana.json --branch 7ce09ce6e8371cb0ed4b --all-definitions --folder-path .
+npx cloudcms-util import -g ../gitana.json --branch d19ead4bc1bd4d4b3007 --all-definitions --folder-path .
+# npx cloudcms-util import -g ../gitana.json --branch master               --all-definitions --folder-path .
