8acb0157f0d58b1014618007c752bba3d9e65455
Harry Moore <harrymoore@yahoo.com>
2020-05-20 20:35:33 -0400

    content model updates

--- content-model/deploy-content-model.sh
+++ content-model/deploy-content-model.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# build form fields from definition
+# npx cloudcms-util create-form-fields --data-path ./ --qname fabric:category --overwrite
+
 # use cloudcms-util import feature to upload a local copy of the content model to a cloud cms branch
 # npm install cloudcms-util
 npx cloudcms-util import -g ../gitana.json --branch 7ce09ce6e8371cb0ed4b --all-definitions --folder-path .
