a00c4a0ff5eb91544a8a7734eec8284aa4a67680
Harry Moore <harrymoore@yahoo.com>
2020-05-27 10:54:05 -0400

    ui module updates

--- ui-modules/ui/index.js
+++ ui-modules/ui/index.js
@@ -1,4 +1,4 @@
 define(function(require, exports, module) {
     require("./scripts/mods.js");
-    require("./content-instances-module/content-instances.js");
+    // require("./content-instances-module/content-instances.js");
 });
