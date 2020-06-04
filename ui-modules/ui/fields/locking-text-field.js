dc8b8502c76a845f3ae848424edc16ef18c1965f
Harry Moore <harrymoore@yahoo.com>
2020-06-04 12:25:23 -0400

    create locking-text field

---
+++ ui-modules/ui/fields/locking-text-field.js
@@ -0,0 +1,42 @@
+(function($) {
+
+    var Alpaca = $.alpaca;
+
+    if (!Alpaca.Fields) {
+        Alpaca.Fields = {};
+    }
+
+    Alpaca.Fields.LockingTextField = Alpaca.Fields.TextField.extend(
+    {
+        setup: function () {
+            var self = this;
+
+            try 
+            {
+                if (Alpaca.globalContext.document.__features()["f:publishable"].state === 'live')
+                {
+                    self.options.readonly = true;
+                }    
+            }
+            catch {}
+
+            self.base();
+        },
+
+        getFieldType: function()
+        {
+            return "locking-text";
+        },
+
+        getTitle: function() {
+            return "Locking Text Field";
+        },
+
+        getDescription: function() {
+            return "Subclass of Text field which locks input if node is Published";
+        }
+    });
+
+    Alpaca.registerFieldClass("locking-text", Alpaca.Fields.LockingTextField);
+
+})(jQuery);
--- ui-modules/ui/index.js
+++ ui-modules/ui/index.js
@@ -1,5 +1,6 @@
 define(function(require, exports, module) {
     require("./fields/picker-field.js");
+    require("./fields/locking-text-field.js");
     // require("./scripts/mods.js");
     // require("./content-instances-module/content-instances.js");
 });
