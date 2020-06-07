528cae9055a7d19a66c1fe51a31bc09e6da2368b
Harry Moore <harrymoore@yahoo.com>
2020-06-07 16:01:34 -0400

    ui module updates

---
+++ ui-modules/ui/gadgets/content-instances.js
@@ -0,0 +1,111 @@
+define(function (require, exports, module) {
+    var $ = require("jquery");
+    var Ratchet = require("ratchet/web");
+    var OneTeam = require("oneteam");
+    var TemplateHelperFactory = require("template-helper");
+
+    var ContentInstancesGadget = require("app/gadgets/project/content/content-instances");
+
+    return Ratchet.GadgetRegistry.register("custom-content-instances", ContentInstancesGadget.extend({
+
+        doGitanaQuery: function (context, model, searchTerm, query, pagination, callback) {
+            if (!query) {
+                query = {};
+            }
+
+            query._fields = {
+                title: 1,
+                description: 1,
+                _system: 1,
+                _type: 1,
+                "mainImage.id": 1,
+                "_features.f:locale.locale": 1,
+                "_features.f:translation.locale": 1
+            };
+
+            if (searchTerm) {
+                Object.assign(query, OneTeam.searchQuery(searchTerm, ["title", "description"]));
+            }
+
+            this.base(context, model, searchTerm, query, pagination, function (resultMap) {
+
+                var array = resultMap.asArray();
+
+                model.size = resultMap.size();
+                model.totalRows = resultMap.totalRows();
+
+                // copy into map so that we can reference by ID
+                // this may help with drag/drop                
+                model.rowsById = {};
+                for (var i = 0; i < array.length; i++) {
+                    var row = array[i];
+                    model.rowsById[row._doc] = row;
+                }
+
+                callback(resultMap);
+            });
+        },
+
+        iconUri: function (row, model, context) {
+            var imageNodeId;
+
+            if (row.mainImage && row.mainImage.id) {
+                imageNodeId = row.mainImage.id;
+            } else {
+                return OneTeam.iconUriForNode(row);
+            }
+
+            return _iconUri = OneTeam.iconUri(this.observable("repository").get()._doc, this.observable("branch").get()._doc, imageNodeId, false, null, { size: 120 }, "image/png");
+        },
+
+        prepareModel: function (el, model, callback) {
+            var self = this;
+
+            this.base(el, model, function () {
+
+                TemplateHelperFactory.create(self, "content-instances", function (err, renderTemplate) {
+
+                    model.renderTemplate = renderTemplate;
+
+                    var project = self.observable("project").get();
+
+                    model.projectId = project.getId();
+                    model.types = [];
+
+                    OneTeam.project2ContentTypes(self, true, function (contentTypeEntries) {
+
+                        var selectedContentTypeDescriptor = null;
+
+                        // select based on incoming qname, otherwise select first
+                        var qname = model.tokens["qname"];
+                        if (qname) {
+                            for (var i = 0; i < contentTypeEntries.length; i++) {
+                                if (contentTypeEntries[i].qname === qname) {
+                                    selectedContentTypeDescriptor = contentTypeEntries[i];
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (!selectedContentTypeDescriptor) {
+                            if (contentTypeEntries && contentTypeEntries.length > 0) {
+                                selectedContentTypeDescriptor = contentTypeEntries[0];
+                            }
+                        }
+
+                        model.selectedContentTypeDescriptor = selectedContentTypeDescriptor;
+
+                        // we set this here in case any dependent code needs this observable
+                        // (such as the create content wizard which links from this page)
+                        if (selectedContentTypeDescriptor) {
+                            self.observable("selected-content-type").set(selectedContentTypeDescriptor);
+                        }
+
+                        callback();
+
+                    });
+                });
+            });
+        }
+    }));
+});
--- ui-modules/ui/scripts/mods.js
+++ ui-modules/ui/scripts/mods.js
@@ -1,92 +1,32 @@
 define(function (require, exports, module) {
     var $ = require("jquery");
 
-    // only register this handler on the Content instances page
-    // if (url && -1 !== url.indexOf('/content/fabric:project') || -1 !== url.indexOf('/documents/')) {
-    //     document.addEventListener("load", function (evt) {
-    //         // an image was loaded. if it looks like the preview image of a content instance then handle it
-    //         var srcUrl = "" + evt.target.src;
-    //         var matches = srcUrl.match(/preview\/([^?]+)\?repository=([^&]+)\&branch=([^&]+)\&node=([^&]+)\&/);
+    // document.addEventListener("load", function (ev) {
+    //     var s = "" + ev.target.src;
+    //     if (s && -1 !== s.indexOf('/preview/') && -1 !== s.indexOf('size=64')) {
+    //         // s = s.replace("name=icon64", "name=icon160");
+    //         s = s.replace("size=64", "size=128");
+    //         // s = s.replace("mimetype=image/png", "mimetype=image/jpeg");
+    //         ev.target.src = s;
+    //     }
+    // }, true);
 
-    //         if (matches && matches.length == 5) {
-    //             // console.log("Node id = " + matches[4] + " srcUrl = " + srcUrl);
-    //             // console.log("event.target.src " + JSON.stringify(evt.target.src));
+    // (function (send) {
+    //     XMLHttpRequest.prototype.send = function (body) {
+    //         var info = "send data\r\n" + body;
+    //         console.log(info);
+    //         send.call(this, body);
+    //     };
+    // })(XMLHttpRequest.prototype.send);
 
-    //             var nodeUrl = "/proxy/repositories/" + matches[2] + "/branches/" + matches[3] + "/nodes/query";
-    //             var query = '{"_doc": "_ID_","_fields": {"mainImage.image.id": 1}}'.replace("_ID_", matches[4]);
+    let oldXHROpen = window.XMLHttpRequest.prototype.open;
+    window.XMLHttpRequest.prototype.open = function (method, url, async, user, password) {
+        // do something with the method, url and etc.
+        this.addEventListener('load', function () {
+            // do something with the response text
+            console.log('load: ' + this.responseText);
+        });
 
-    //             // see if this is a fabric:project icon preview request
-    //             $.ajax({
-    //                 type: "GET",
-    //                 url: nodeUrl,
-    //                 data: {
-    //                     "query": query
-    //                 },
-    //                 context: evt.target
-    //             }).done(function (data) {
-    //                 if (data && data.rows && data.rows.length && data.rows.length > 0 && data.rows[0].mainImage && data.rows[0].mainImage.image && data.rows[0].mainImage.image.id) {
-    //                     var targetEl = this;
-    //                     try {
-    //                         // attempt to replace the icon preview url with a url that refers to the node's mainImage property
-    //                         // targetEl.src = "/proxy/repositories/_REPO_/branches/_BRANCH_/nodes/_NODE_/attachments/default?size=128".replace("_REPO_", matches[2]).replace("_BRANCH_", matches[3]).replace("_NODE_", data.rows[0].mainImage.image.id);
-    //                         targetEl.src = "/preview/_NODE1_?repository=_REPO_&branch=_BRANCH_&node=_NODE2_&mimetype=image/png&size=128&name=preview128&attachment=default".replace("_REPO_", matches[2]).replace("_BRANCH_", matches[3]).replace("_NODE1_", matches[4]).replace("_NODE2_", data.rows[0].mainImage.image.id);
-    //                     } finally { }
-    //                 }
-    //             });
-    //         }
-    //     }, true);
-    // }
-
-    // use larger image previews
-    document.addEventListener("load", function(ev) {
-        var url = "" + location.href;
-
-        if (url && -1 !== url.indexOf('/content/fabric:project') || -1 !== url.indexOf('/documents/') && -1 === url.indexOf('&_replaced')) {
-            // an image was loaded. if it looks like the preview image of a content instance then handle it
-            // var srcUrl = "" + ev.target.src;
-            // var matches = srcUrl.match(/preview\/([^?]+)\?repository=([^&]+)\&branch=([^&]+)\&node=([^&]+)\&/);
-
-            // if (matches && matches.length == 5) {
-            //     // console.log("Node id = " + matches[4] + " srcUrl = " + srcUrl);
-            //     // console.log("event.target.src " + JSON.stringify(ev.target.src));
-
-            //     var nodeUrl = "/proxy/repositories/" + matches[2] + "/branches/" + matches[3] + "/nodes/query";
-            //     var query = '{"_doc": "_ID_","_fields": {"mainImage": 1}}'.replace("_ID_", matches[4]);
-
-            //     // see if this is a fabric:project icon preview request
-            //     $.ajax({
-            //         type: "GET",
-            //         url: nodeUrl,
-            //         data: {
-            //             "query": query
-            //         },
-            //         context: ev.target
-            //     }).done(function (evt) {
-            //         var target = this;
-            //         if (data && data.rows && data.rows.length && data.rows.length > 0 && data.rows[0].mainImage && data.rows[0].mainImage.image && data.rows[0].mainImage.image.id) {
-            //             try {
-            //                 // attempt to replace the icon preview url with a url that refers to the node's mainImage property
-            //                 var matches = target.src.match(/preview\/([^?]+)\?repository=([^&]+)\&branch=([^&]+)\&node=([^&]+)\&/);
-            //                 var s = "" + target.src;
-            //                 s = s.replace("name=icon64", "name=fabricicon128&force=true&_replaced=true");
-            //                 s = s.replace("size=64", "size=128");
-            //                 s = s.replace("mimetype=image/png", "mimetype=image/jpeg");
-            //                 s = s.replace(/\&node=([^&]+)\&/, "&node=" + data.rows[0].mainImage.image.id + "&")
-            //                 target.src = s;
-            //             } finally { }
-            //         }
-            //     });
-            // }
-        } else {
-            var s = "" + ev.target.src;
-            if (s && -1 !== s.indexOf('.cloudcms.net/preview/') && -1 !== s.indexOf('size=64') && -1 === s.indexOf('&_replaced')) {
-                s = s.replace("name=icon64", "name=fabricicon128&force=true&_replaced=true");
-                s = s.replace("size=64", "size=128");
-                s = s.replace("mimetype=image/png", "mimetype=image/jpeg");
-                // s += "&force=true&_replaced=true";
-                ev.target.src = s;
-            }            
-        }
-    }, true);
-
+        return oldXHROpen.apply(this, arguments);
+    }
 });
