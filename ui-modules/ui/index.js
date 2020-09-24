03d7666ccc7925f13d891d17bbb41ddc62f20dfd
Harry Moore <harrymoore@yahoo.com>
2020-09-24 16:40:28 -0400

    enable script in custom module

--- ui-modules/ui/index.js
+++ ui-modules/ui/index.js
@@ -2,5 +2,5 @@ define(function(require, exports, module) {
     require("./fields/picker-field.js");
     require("./fields/locking-text-field.js");
     require("./gadgets/content-instances.js");
-    // require("./scripts/mods.js");
+    require("./scripts/mods.js");
 });
