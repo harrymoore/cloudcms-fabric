0207ca0a9a51f26847f03db6f806694652bc7a3e
Harry Moore <harrymoore@yahoo.com>
2020-09-30 12:54:28 -0400

    ui config updates

--- ui-modules/ui/scripts/mods.js
+++ ui-modules/ui/scripts/mods.js
@@ -4,8 +4,8 @@ define(function (require, exports, module) {
     // use larger image previews
     document.addEventListener("load", function(ev) {
         var s = "" + ev.target.src;
-        if (s && -1 !== s.indexOf('.cloudcms.net/preview/') && -1 !== s.indexOf('name=iconicondvor128') && -1 !== s.indexOf('size=128')) {
-            s = s.replace("name=icon64", "name=iconicondvor128");
+        if (s && -1 !== s.indexOf('.cloudcms.net/preview/') && -1 !== s.indexOf('name=iconfabric128') && -1 !== s.indexOf('size=128')) {
+            s = s.replace("name=icon64", "name=iconfabric128");
             s = s.replace("size=64", "size=128");
             s = s.replace("mimetype=image/png", "mimetype=image/jpeg");
             ev.target.src = s;
