6fa4c9706d6fba2201e11dd62373f00174bcb9d1
Harry Moore <harrymoore@yahoo.com>
2020-10-08 09:55:36 -0400

    Category and Keywords are now imported with blog content. Update README

--- content-model/deploy-content-model.sh
+++ content-model/deploy-content-model.sh
@@ -5,5 +5,7 @@
 
 # use cloudcms-util import feature to upload a local copy of the content model to a cloud cms branch
 # npm install cloudcms-util
-npx cloudcms-util import -g ../gitana-fabric.json --branch master --all-definitions --folder-path .
+# npx cloudcms-util import -g ../gitana-fabric.json --branch master --all-definitions --folder-path .
+# npx cloudcms-util import -g ../gitana-harry.json --branch master --all-definitions --folder-path .
+npx cloudcms-util import -g ../gitana-harry2.json --branch master --all-definitions --folder-path .
 # npx cloudcms-util import -g ../gitana.json --branch 85418f221a70809bf113 --all-definitions --folder-path .
